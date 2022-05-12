d7651c431fce920f613bae8210775c3c7aaafe54
tianyan071128 <1033051985@qq.com>
2022-05-12 17:31:09 +0800

    init

--- README.md
+++ README.md
@@ -1,24 +1,4 @@
-# vue2_mobile
+# vue2_mobile 
 
-## Project setup
-```
-npm install
-```
+适用于 vue2 的移动端
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="test">点击</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  methods: {
+    test() {
+      // const a = aaa;
+      console.log(process.env);
+      console.warn(process.env);
+      console.error(process.env);
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $test;
 }
 </style>
